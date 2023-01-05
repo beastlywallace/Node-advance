a247d96448a36ba627efb107370cbe610ff28725
beastlywallace <akonakpevwe@gmail.com>
2023-01-05 09:44:40 +0100

    sort

--- store-api/starter/controllers/products.js
+++ store-api/starter/controllers/products.js
@@ -2,14 +2,45 @@ const Product = require('../models/products')
 
 const getAllProductsStatic = async (req, res) => {
     // throw new Error('testing async errors')
-    const products = await Product.find({featured: false})
-    console.log(products)
+// const {name}= req.body
+    const products = await Product.find({}).sort('-name').select('name price') 
+    //we have .select('name price')  meaning only the name will com out
+
     res.status(200).json({ products, nbHits: products.length})
 }
 const getAllProducts = async (req, res) => {
-    const products= Product.find(req.query)
+    //for query
+    const { featured, company, name , fields, sort} = req.query
+    const queryObject={}
+   
     
-    res.status(200).json({msg: "products  route"})
+    if (featured) {
+        queryObject.featured = featured ==='true' ? true: false
+    }
+    if (company) {
+        queryObject.company = company
+    }
+    if (name) {
+        queryObject.name= {$regex: name, $option: i}
+    }
+
+    let result = Product.find(queryObject)
+    //sort
+    if (sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    } else {
+        result = result.sort(createdAt)
+    }
+
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(sortList)
+    }
+
+     const products= Product.find(queryObject)
+      res.status(200).json({ products, nbHits: products.length})
+    
 }
 
 module.exports = {
